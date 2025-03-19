57581dfddafb30c6324b57bcae312ba418db0c27
Ayesha <aishey082@gmail.com>
2025-03-19 20:02:43 +0500

    updated

--- src/custom_agents/main.py
+++ src/custom_agents/main.py
@@ -16,16 +16,13 @@ from custom_agents.custom_tools.reservation_tool import handle_reservation
 load_dotenv()
 
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
-GOOGLE_API_KEY_1 = os.getenv("GOOGLE_API_KEY_1")
-GOOGLE_API_KEY_2 = os.getenv("GOOGLE_API_KEY_2")
-GOOGLE_API_KEY_3 = os.getenv("GOOGLE_API_KEY_3")
 
 
 @cl.on_chat_start
 async def start():
     #Reference: https://ai.google.dev/gemini-api/docs/openai
     external_client = AsyncOpenAI(
-        api_key=GOOGLE_API_KEY_1,
+        api_key=GEMINI_API_KEY,
         base_url="https://generativelanguage.googleapis.com/v1beta/openai/",
     )
 
